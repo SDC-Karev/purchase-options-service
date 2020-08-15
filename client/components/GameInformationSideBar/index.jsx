9eab69f81bb437544faef7ebe79407d4731e28d4
Tristan Smith <tristan.an.smith@gmail.com>
2020-08-14 20:45:50 -0400

    Implemented Game Languages, Rating and Share Buttons

--- client/components/GameInformationSideBar/index.jsx
+++ client/components/GameInformationSideBar/index.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 import styles from '../../style.css';
 
 import GameCategory from './GameCategories.jsx'
+import GameLanguages from './GameLanguages.jsx'
+import GameRating from './GameRating.jsx'
+import ShareBlock from './ShareBlock.jsx'
+
+
+const temporaryLanguages = {
+  English: [1, 1, 1],
+  French: [1, 1, 1],
+  Italian: [1, 1, 1],
+  German: [1, 1, 1],
+  "Spanish - Spain": [1, 1, 1],
+  Polish: [1, 1, 1],
+  "Traditional Chinese": [1, 0, 1],
+  Russian: [1, 1, 1],
+  Japanese: [1, 1, 1],
+};
+
+const temporaryGameRating = {
+  details: 'BLOOD AND GORE, INTENSE VIOLENCE, SEXUAL THEMES, USE OF ALCOHOL',
+  img: 'https://store.cloudflare.steamstatic.com/public/shared/images/game_ratings/ESRB/m.png',
+};
 
 const GameInformationSideBar = ({ game }) => (
   <div className={styles.game_information}>
-    <div className={styles.right_info_block}>
-      <img src="#" alt="Information regarding the game" />
-    </div>
-    <div className={styles.right_info_block}>
+
+    <div>
       <GameCategory
         target={"#"}
         img={"https://store.cloudflare.steamstatic.com/public/images/v6/ico/ico_singlePlayer.png"}
@@ -46,27 +65,20 @@ const GameInformationSideBar = ({ game }) => (
             </div>
       </div>
     </div>
-    <div className={styles.right_info_block}>
-      <img src="#" alt="Information regarding the game" />
+
+    <div>
+      <GameLanguages languages={temporaryLanguages} />
     </div>
-    <div className={styles.right_info_block}>
-      <img src="#" alt="Information regarding the game" />
+
+    <div>
+      <GameRating details={temporaryGameRating.details} img={temporaryGameRating.img} />
     </div>
-    <div className={styles.right_info_block}>
-      <img src="#" alt="Information regarding the game" />
+
+    <div>
+      <ShareBlock />
     </div>
-    <div className={styles.right_info_block}>
-      <img src="#" alt="Information regarding the game" />
-    </div>
-    <div className={styles.right_info_block}>
-      <img src="#" alt="Information regarding the game" />
-    </div>
-    <div className={styles.right_info_block}>
-      <img src="#" alt="Information regarding the game" />
-    </div>
-    <div className={styles.right_info_block}>
-      <img src="#" alt="Information regarding the game" />
-    </div>
+
+
   </div>
 );
 
