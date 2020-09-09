c2144d89f83323053f157f72a8001ef29bab0275
Evan Kolb <kongaiwen@gmail.com>
2020-09-09 15:38:07 -0700

    Change the way the gameId is pulled from the window pathname

--- client/index.jsx
+++ client/index.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PurchaseOptions from './PurchaseOptions';
 
-const paths = window.location.pathname.split('/');
-const gameId = paths[paths.length - 2];
+// const paths = window.location.pathname.split('/');
+// const gameId = paths[paths.length - 2];
+
+const pathToId = window.location.pathname;
+const reg = /[0-9]+/g;
+const id = pathToId.match(reg);
+const gameId = id[0];
+
 ReactDOM.render(<PurchaseOptions gameId={gameId} />, document.getElementById('purchase-options'));
